5aa373cb36a6507bf05fa42d68eab712160a8ef0
shan-Boris <shanboris@gmail.com>
2022-04-03 18:34:00 +0700

    'Refactoring_spoiler'

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
 'use strict'; 
+import spoiler from './modules/spoiler';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    const queryAll = document.querySelectorAll('.faq__query');
-    queryAll.forEach((v, i) => {v.addEventListener('click', () => {
-        const queryPlus = queryAll[i].querySelector('.faq__plus');
-        if (queryPlus.classList.contains('faq__plusToCross')) {
-            queryPlus.classList.remove('faq__plusToCross');
-            queryPlus.classList.add('faq__crossToPlus');
-        } else {
-            queryPlus.classList.remove('faq__crossToPlus');
-            queryPlus.classList.add('faq__plusToCross');
-        }
-
-    queryAll[i].querySelector('.faq__answer').classList.toggle('faq__answer_show');
-    
-    })})
+    spoiler();
 
 })
