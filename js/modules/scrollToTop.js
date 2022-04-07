54c1131600ef787c96a4493507c10a2413f4fa3a
shan-Boris <shanboris@gmail.com>
2022-04-07 22:02:33 +0700

    'Add_scrollToTop_with_progressbar'

---
+++ js/modules/scrollToTop.js
@@ -0,0 +1,26 @@
+function addScrollToTopWithPG() {
+    const offset = 200, // height for emersion 
+    scrollUp = document.querySelector('.scroll-up'),
+    scrollUpSvgPath = document.querySelector('.scroll-up__svg-path'),
+    pathLength = scrollUpSvgPath.getTotalLength(),
+    heightDoc = document.documentElement.scrollHeight - window.innerHeight;
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+function updateProgressBar() {
+  const passedPath = window.scrollY * pathLength / heightDoc;
+  scrollUpSvgPath.style.strokeDashoffset = pathLength - passedPath;
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > offset) {
+      scrollUp.classList.add('scroll-up_activ');
+      updateProgressBar();
+  } else {
+      scrollUp.classList.remove('scroll-up_activ');
+  }
+})
+};
+
+export default addScrollToTopWithPG();
